b183697361f9b5a0b8a9374da33337174de36270
HuanDo22222 <huando22222@gmail.com>
2024-07-20 20:39:19 +0700

    upload new post

--- middlewares/post-middleware.js
+++ middlewares/post-middleware.js
@@ -1,12 +1,14 @@
 const path = require("path");
 const multer = require("multer");
+const fs = require("fs");
 
 const createMulterMiddleware = (uploadPath, fileName) => {
 	const storage = multer.diskStorage({
 		destination: function (req, file, cb) {
-			cb(null, path.join(__dirname, uploadPath));
+			cb(null, uploadPath);
 		},
 		filename: function (req, file, cb) {
+			console.log("Uploading file:", file.originalname);
 			cb(null, `${fileName}_${file.originalname}`);
 		},
 	});
@@ -14,34 +16,43 @@ const createMulterMiddleware = (uploadPath, fileName) => {
 	return multer({ storage });
 };
 
-// const examplePath = "../public/images";
-// const exampleName = Date.now();
 
 module.exports = {
     UploadPhoto: (req, res, next) => {
-        try {
-            const { examplePath, exampleName } = req.body; //tam thoi thoi //truyen vao header roi gui về server
-			//path = phone  , name = phone+date
-            if (!examplePath || !exampleName) {
-                console.log("empty path , name");
-                return res
+		try {
+			//tam thoi thoi //truyen vao header roi gui về server
+			//path = phone  , name = phone+date => hash da~
+			const phone = req.tokenPayload.phone; //folder
+			
+			
+			console.log("Uploading phôto");
+			if (!phone) {
+				console.log("empty path , name");
+				return res
 					.status(400)
 					.json({ error: "examplePath or exampleName is missing" });
 			}
-            const uploadPhoto = createMulterMiddleware(
-				examplePath,
-				exampleName
-			);
+
+			const uploadPath = path.join(__dirname, "../public/images/users", phone);
+			if (!fs.existsSync(uploadPath)) {
+				// Create the directory
+				fs.mkdirSync(uploadPath, { recursive: true });
+			}
+
+			const fileName = `${phone}_${Date.now()}`;
+			const uploadPhoto = createMulterMiddleware(uploadPath , fileName);
 
 			uploadPhoto.single("image")(req, res, (err) => {
 				if (err instanceof multer.MulterError) {
+					console.log(err);
 					return res.status(400).json({ error: "Upload failed" });
 				} else if (err) {
+					console.log(err);
 					return res.status(500).json({ error: "Server error" });
 				}
 				next();
 			});
-        } catch (error) {
+		} catch (error) {
             next(error);
         }
 	},
